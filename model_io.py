960b3ae9cfa02d6ac3a4aedd97b685c064879f46
Sourideb <souridebnew9@gmail.com>
2026-03-21 14:55:59 +0530

    changed the load_checkpoint function for resolving parallel gpu training error

--- model_io.py
+++ model_io.py
@@ -32,7 +32,7 @@ def load_weights(model, filename, path="./saved_models"):
     model.load_state_dict(state_dict)
     return model
 
-
+"""
 def load_checkpoint(fpath, model, optimizer=None):
     ckpt = torch.load(fpath, map_location='cpu')
     if optimizer is None:
@@ -69,4 +69,46 @@ def load_checkpoint(fpath, model, optimizer=None):
             modified[k] = v  # else keep the original
 
     model.load_state_dict(modified) #use strict=False when using adabins weights on swin architecture
+    return model, optimizer, epoch
+"""
+
+def load_checkpoint(fpath, model, optimizer=None):
+    ckpt = torch.load(fpath, map_location='cpu')
+    if optimizer is None:
+        optimizer = ckpt.get('optimizer', None)
+    else:
+        optimizer.load_state_dict(ckpt['optimizer'])
+    epoch = ckpt['epoch']
+
+    if 'model' in ckpt:
+        ckpt = ckpt['model']
+
+    load_dict = {}
+    for k, v in ckpt.items():
+        if k.startswith('module.'):
+            k_ = k.replace('module.', '')
+            load_dict[k_] = v
+        else:
+            load_dict[k] = v
+
+    modified = {}
+    for k, v in load_dict.items():
+        if k.startswith('adaptive_bins_layer.embedding_conv.'):
+            k_ = k.replace('adaptive_bins_layer.embedding_conv.',
+                           'adaptive_bins_layer.conv3x3.')
+            modified[k_] = v
+        elif k.startswith('adaptive_bins_layer.patch_transformer.embedding_encoder'):
+            k_ = k.replace('adaptive_bins_layer.patch_transformer.embedding_encoder',
+                           'adaptive_bins_layer.patch_transformer.embedding_convPxP')
+            modified[k_] = v
+        else:
+            modified[k] = v
+
+    # ---- NEW: re-add 'module.' prefix if model is wrapped in DataParallel ----
+    is_data_parallel = isinstance(model, torch.nn.DataParallel)
+    if is_data_parallel:
+        modified = {'module.' + k: v for k, v in modified.items()}
+    # --------------------------------------------------------------------------
+
+    model.load_state_dict(modified)
     return model, optimizer, epoch
